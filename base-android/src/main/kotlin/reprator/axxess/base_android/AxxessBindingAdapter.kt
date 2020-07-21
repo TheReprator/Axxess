d94aae62d6057a69d4bf0f9319817d2ee640c162
mr.vikram004@gmail.com <vikram90#>
2020-07-22 01:09:28 +0530

    proper sizes for landscape and portrait mode

--- base-android/src/main/kotlin/reprator/axxess/base_android/AxxessBindingAdapter.kt
+++ base-android/src/main/kotlin/reprator/axxess/base_android/AxxessBindingAdapter.kt
@@ -1,7 +1,6 @@
 package reprator.axxess.base_android
 
 import android.graphics.drawable.Drawable
-import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import androidx.annotation.DrawableRes
