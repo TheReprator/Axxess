afb81ed1cb40a50d2b1776d9af154bbb4d468ce8
mr.vikram004@gmail.com <vikram90#>
2020-07-22 05:09:54 +0530

    unit testing for SearchUseCaseTest.kt complete

--- search/src/test/kotlin/reprator/axxess/search/domain/usecase/SearchUseCaseTest.kt
+++ search/src/test/kotlin/reprator/axxess/search/domain/usecase/SearchUseCaseTest.kt
@@ -2,11 +2,12 @@ package reprator.axxess.search.domain.usecase
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.google.common.truth.Truth
-import io.mockk.*
+import io.mockk.MockKAnnotations
+import io.mockk.clearAllMocks
+import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.impl.annotations.MockK
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.*
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
@@ -16,7 +17,6 @@ import reprator.axxess.base.util.useCases.Success
 import reprator.axxess.base_android.SearchModal
 import reprator.axxess.search.TestCoroutineRule
 import reprator.axxess.search.domain.repository.SearchRepository
-import java.lang.Exception
 
 class SearchUseCaseTest {
 
@@ -40,6 +40,25 @@ class SearchUseCaseTest {
     @After
     fun tearDown() {
         clearAllMocks()
+    }
+
+    @Test
+    fun `no value emitted`() {
+        testCoroutineRule.runBlockingTest {
+
+            val successResponse = Success(emptyList<SearchModal>())
+
+            coEvery {
+                searchUseCase("apple")
+            } returns flow {
+                successResponse
+            }
+
+            val result = searchUseCase("apple")
+
+            val flowsWithContentCombined = result.count()
+            Truth.assertThat(flowsWithContentCombined).isAtLeast(0)
+        }
     }
 
     @Test
@@ -93,10 +112,12 @@ class SearchUseCaseTest {
     fun `get list of success data`() {
         testCoroutineRule.runBlockingTest {
 
-            val successResponse = Success(listOf(
-                SearchModal("1","apple","appleUrl"),
-                SearchModal("2","apple pne","appleUrl pine")
-            ))
+            val successResponse = Success(
+                listOf(
+                    SearchModal("1", "apple", "appleUrl"),
+                    SearchModal("2", "apple pne", "appleUrl pine")
+                )
+            )
 
             coEvery {
                 searchUseCase("apple")
@@ -104,18 +125,60 @@ class SearchUseCaseTest {
                 emit(successResponse)
             }
 
+            val result = searchUseCase("apple")
+
+            result.collect {
+                Truth.assertThat(it).isEqualTo(successResponse)
+                Truth.assertThat(it.get()).hasSize(2)
+            }
+        }
+    }
+
+    @Test
+    fun `get list of success data comparison`() {
+        testCoroutineRule.runBlockingTest {
+
+            val searchModalList = listOf(
+                SearchModal("1", "apple", "appleUrl"),
+                SearchModal("2", "apple pne", "appleUrl pine"))
+
             coEvery {
-                searchRepository.searchItems("apple")
+                searchUseCase("apple")
             } returns flow {
-                throw  Exception("vvvv")
+                emit(Success(searchModalList))
             }
 
             val result = searchUseCase("apple")
 
+            val searchModalListDynamic = mutableListOf<SearchModal>()
             result.collect {
-                    Truth.assertThat(it).isEqualTo(successResponse)
-                    Truth.assertThat(it.get()).hasSize(2)
-                }
+                searchModalListDynamic+= it.get()!!
+            }
+
+            Truth.assertThat(searchModalList).isEqualTo(searchModalListDynamic)
+        }
+    }
+
+    @Test
+    fun `get list of success data from respositories`() {
+        testCoroutineRule.runBlockingTest {
+
+            val searchModalList = listOf(
+                SearchModal("1", "apple", "appleUrl"),
+                SearchModal("2", "apple pne", "appleUrl pine"))
+
+            coEvery {
+                searchRepository.searchItems("apple")
+            } returns flow {
+                emit(Success(searchModalList))
+            }
+
+            val result = searchRepository.searchItems("apple")
+
+            val searchModalListDynamic = mutableListOf<SearchModal>()
+            result.collect {
+                searchModalListDynamic+= it.get()!!
+            }
         }
     }
 }
